934a772808575eb83aafe84e0866119eb2cbc5f5
Sathyaapurushoth <94157563+Sathyaapurushoth@users.noreply.github.com>
2023-09-09 22:54:52 +0530

    Update urls.py

--- mcq/urls.py
+++ mcq/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from .views import Mcq, RandomQuestion, McqQuestion
 
+app_name='Mcq'
+
+urlpatterns = [
+    path('', Mcq.as_view(), name='Mcq'),
+    path('r/<str:topic>/', RandomQuestion.as_view(), name='random' ),
+    path('q/<str:topic>/', McqQuestion.as_view(), name='questions' ),
+]
