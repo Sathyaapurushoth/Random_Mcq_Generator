0839222090be7641cd41b077efbb42c86e7c9ffb
Sathyaapurushoth <94157563+Sathyaapurushoth@users.noreply.github.com>
2023-09-09 22:55:51 +0530

    Update views.py

--- mcq/views.py
+++ mcq/views.py
@@ -1,1 +1,26 @@
+#from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Mcqs, Question
+from .serializers import McqSerializer, RandomQuestionSerializer, QuestionSerializer
+from rest_framework.views import APIView
 
+class Mcq(generics.ListAPIView):
+
+    serializer_class = McqSerializer
+    queryset = Mcqs.objects.all()
+
+    
+class RandomQuestion(APIView):
+
+    def get(self, request, format=None, **kwargs):
+        question = Question.objects.filter(mcq__title=kwargs['topic']).order_by('?')[:1]
+        serializer = RandomQuestionSerializer(question, many=True)
+        return Response(serializer.data)
+
+class McqQuestion(APIView):
+
+    def get(self, request, format=None, **kwargs):
+        question = Question.objects.filter(mcq__title=kwargs['topic'])
+        serializer = QuestionSerializer(question, many=True)
+        return Response(serializer.data)
